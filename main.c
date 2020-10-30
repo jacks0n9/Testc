637dc355cc0daa2bbd0f0e41b8d6b85f38230343
elnepik <67214151+elnepik@users.noreply.github.com>
2020-10-30 10:26:54 -0700

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 int main(){
+	printf("I'm just working on this so I can have a project in the arctic code vault lol");
 	for(int i=0;i<=100;i++){
 		printf("Number: %i\n ", i);
 
